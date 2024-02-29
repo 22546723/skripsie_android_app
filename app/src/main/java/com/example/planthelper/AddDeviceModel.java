3691eb860f958f8d6aeb65602f689c371a36467b
22546723 <22546723@sun.ac.za>
2024-02-29 16:03:54 +0200

    started working on add device page

---
+++ app/src/main/java/com/example/planthelper/AddDeviceModel.java
@@ -0,0 +1,17 @@
+package com.example.planthelper;
+
+public class AddDeviceModel {
+    private String device_name;
+
+    public AddDeviceModel(String device_name) {
+        this.device_name = device_name;
+    }
+
+    public String getDevice_name() {
+        return this.device_name;
+    }
+
+    public void setDevice_name(String device_name) {
+        this.device_name = device_name;
+    }
+}
