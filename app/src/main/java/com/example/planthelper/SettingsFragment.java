7e27655bd3cf900cade65f0a6bbd7db0c09ed382
22546723 <22546723@sun.ac.za>
2024-03-05 13:31:25 +0200

    settings navigation and gui done

--- app/src/main/java/com/example/planthelper/SettingsFragment.java
+++ app/src/main/java/com/example/planthelper/SettingsFragment.java
@@ -4,16 +4,27 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.planthelper.databinding.FragmentSettingsBinding;
 
 
 public class SettingsFragment extends Fragment {
     private FragmentSettingsBinding binding;
+    private Button btnSave, btnDelete;
+    private ImageButton ibtnName, ibtnWifi;
+    private EditText edtName;
+    private TextView tvWifi;
+
 
     @Override
     public View onCreateView(
@@ -28,6 +39,31 @@ public class SettingsFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        btnSave = binding.btnSaveSettings;
+        btnDelete = binding.btnDeleteDevice;
+        ibtnName = binding.ibtnEditName;
+        ibtnWifi = binding.ibtnEditWifi;
+        edtName = binding.edtDeviceName;
+        tvWifi = binding.tvWifiName;
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Todo: Add code to store data
+                NavController navController = Navigation.findNavController(v);
+                navController.navigate(R.id.action_settingsFragment_to_controlPanel);
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Todo: Add code to delete device data
+                NavController navController = Navigation.findNavController(v);
+                navController.navigate(R.id.action_settingsFragment_to_addDeviceFragment);
+            }
+        });
 
     }
 
