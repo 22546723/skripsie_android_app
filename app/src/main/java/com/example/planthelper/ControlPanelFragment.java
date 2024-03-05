aaaa6be807dc9df14e47c55363883f9955d93d18
22546723 <22546723@sun.ac.za>
2024-03-05 11:31:25 +0200

    finished control panel design

--- app/src/main/java/com/example/planthelper/ControlPanelFragment.java
+++ app/src/main/java/com/example/planthelper/ControlPanelFragment.java
@@ -19,7 +19,7 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 public class ControlPanelFragment extends Fragment {
 
     private FragmentControlPanelBinding binding;
-    //private GraphView graphView;
+    private GraphView graphView;
 
     @Override
     public View onCreateView(
@@ -35,8 +35,8 @@ public class ControlPanelFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-       // graphView = binding.idGraphView;
-        //testGraph();
+        graphView = binding.idGraphView;
+        testGraph();
     }
 
     @Override
@@ -45,36 +45,36 @@ public class ControlPanelFragment extends Fragment {
         binding = null;
     }
 
-//    private void testGraph() {
-//        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
-//                // on below line we are adding
-//                // each point on our x and y axis.
-//                new DataPoint(0, 1),
-//                new DataPoint(1, 3),
-//                new DataPoint(2, 4),
-//                new DataPoint(3, 9),
-//                new DataPoint(4, 6),
-//                new DataPoint(5, 3),
-//                new DataPoint(6, 6),
-//                new DataPoint(7, 1),
-//                new DataPoint(8, 2)
-//        });
-//
-//        // after adding data to our line graph series.
-//        // on below line we are setting
-//        // title for our graph view.
-//        graphView.setTitle("My Graph View");
-//
-//        // on below line we are setting
-//        // text color to our graph view.
-////        graphView.setTitleColor(R.color.purple_200);
-//
-//        // on below line we are setting
-//        // our title text size.
-//        graphView.setTitleTextSize(18);
-//
-//        // on below line we are adding
-//        // data series to our graph view.
-//        graphView.addSeries(series);
-//    }
+    private void testGraph() {
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+                // on below line we are adding
+                // each point on our x and y axis.
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 9),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6),
+                new DataPoint(7, 1),
+                new DataPoint(8, 2)
+        });
+
+        // after adding data to our line graph series.
+        // on below line we are setting
+        // title for our graph view.
+        graphView.setTitle("My Graph View");
+
+        // on below line we are setting
+        // text color to our graph view.
+//        graphView.setTitleColor(R.color.purple_200);
+
+        // on below line we are setting
+        // our title text size.
+        graphView.setTitleTextSize(18);
+
+        // on below line we are adding
+        // data series to our graph view.
+        graphView.addSeries(series);
+    }
 }
