17f5a6d4edf81b67d5c0026f979c74b9d14e0f63
22546723 <22546723@sun.ac.za>
2024-04-08 15:21:47 +0200

    busy with commenting and cleanup

--- app/src/main/java/com/example/planthelper/BluetoothManager.java
+++ app/src/main/java/com/example/planthelper/BluetoothManager.java
@@ -1,14 +1,7 @@
 package com.example.planthelper;
 
-import static androidx.core.app.ActivityCompat.startActivityForResult;
-
-//import static com.example.planthelper.MainActivity.REQUEST_ENABLE_BT;
-
-import static com.google.android.gms.common.util.ArrayUtils.contains;
-
 import android.Manifest;
 import android.app.Activity;
-import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
@@ -17,71 +10,45 @@ import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
-import android.bluetooth.le.ScanRecord;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Binder;
 import android.os.Build;
 import android.os.Handler;
-import android.os.IBinder;
-import android.os.ParcelUuid;
 import android.util.Log;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
-//class BluetoothLeService extends Service {
-//    private Binder binder = new LocalBinder();
-//
-//    @Nullable
-//    @Override
-//    public IBinder onBind(Intent intent) {
-//        return binder;
-//    }
-//
-//    class LocalBinder extends Binder {
-//        public BluetoothLeService getService() {
-//            return BluetoothLeService.this;
-//        }
-//    }
-//}
 
 @RequiresApi(api = Build.VERSION_CODES.S)
 public class BluetoothManager extends AppCompatActivity {
     private static final int REQUEST_ENABLE_BT = 1;
-    private BluetoothAdapter bluetoothAdapter;
     private BluetoothLeScanner bluetoothLeScanner;
-    private Handler handler = new Handler();
-    private boolean hasScanner = false;
+    private final Handler handler = new Handler();
     private boolean scanning = false;
-    private final long SCAN_PERIOD = 15000; // 10 sec
     private BluetoothLeService bluetoothLeService;
     private BluetoothDevice device;
-    private ScanFilter scanFilter;
-    private ScanSettings scanSettings;
+    private final ScanFilter scanFilter;
+    private final ScanSettings scanSettings;
     private BluetoothGatt bluetoothGatt;
 
-    private Context context;
-    private Activity activity;
+    private final Context context;
+    private final Activity activity;
 
-    //    private String[] permissionNames = {Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_ADVERTISE, Manifest.permission.BLUETOOTH_CONNECT};
-    private String[] permissionNames = {android.Manifest.permission.BLUETOOTH_SCAN, android.Manifest.permission.BLUETOOTH_ADVERTISE, Manifest.permission.BLUETOOTH_CONNECT};
-    private static int[] permissionStatus = {0, 0, 0};
+    private final String[] permissionNames = {android.Manifest.permission.BLUETOOTH_SCAN, android.Manifest.permission.BLUETOOTH_ADVERTISE, Manifest.permission.BLUETOOTH_CONNECT};
+    private static final int[] permissionStatus = {0, 0, 0};
+
+
+    private boolean isConnected = false;
 
 
     private final String SERVICE_UUID_SCAN = "e1f6fa49-170d-4629-bd77-ea170a0309dc";
@@ -99,20 +66,25 @@ public class BluetoothManager extends AppCompatActivity {
     private final String CHARACTERISTIC_UUID_DEVICE = "9fbf707f-c39a-418a-828c-6e291af51ac4";
 
 
+    /**
+     * Class that manages all Bluetooth interaction with the ESP32
+     *
+     * @param c Context to use.
+     * @param a Activity to use.
+     */
     public BluetoothManager(Context c, Activity a) {
         //init
         this.context = c;
         this.activity = a;
-        if (c == null)
-            Log.i("MINE", "empty context");
 
+        // Check permissions
         for (int i = 0; i < permissionNames.length; i++) {
             checkPermission(permissionNames[i], i);
         }
 
-        // filter scan results to contain the scan service uuid
+        // filter scan results to the device id
+        // TODO:
         ScanFilter.Builder scanBuilder = new ScanFilter.Builder();
-//        scanBuilder.setServiceUuid(ParcelUuid.fromString(SERVICE_UUID_CONNECT));
         scanBuilder.setDeviceAddress("40:4C:CA:41:59:0E");
         scanFilter = scanBuilder.build();
 
@@ -133,29 +105,20 @@ public class BluetoothManager extends AppCompatActivity {
 
 
         // setup bt adapter
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter.isEnabled()) {
-
             bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
-            hasScanner = true;
         } else {
             // Show turn on BT message
             Log.i("MINE", "bt not turned on");
-//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-////            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-//
-//            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(enableBtIntent), new ActivityResultCallback<>() {
-//                @Override
-//                public void onActivityResult(Object o) {
-//                    Log.i("MINE", "bt on requested");
-//                }
-//            });
         }
     }
 
     public void scanForDevice() {
         if (!scanning) {
             // add a runnable to the handler that checks bt permission and stops the BT scan after SCAN_PERIOD milliseconds
+            // 10 sec
+            long SCAN_PERIOD = 15000;
             handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
@@ -189,7 +152,7 @@ public class BluetoothManager extends AppCompatActivity {
 
     // bt scan callback
     // so far it just returns the detected device
-    private ScanCallback leScanCallback = new ScanCallback() {
+    private final ScanCallback leScanCallback = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
@@ -231,27 +194,35 @@ public class BluetoothManager extends AppCompatActivity {
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
             if (newState == BluetoothProfile.STATE_CONNECTED) {
                 // successfully connected to the GATT Server
+                isConnected = true;
                 Log.i("MINE", "connected to esp");
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
                 // disconnected from the GATT Server
+                isConnected = false;
             }
         }
     };
 
 
-
     // returns true if phone is connected to the esp32
     public boolean checkConnected() {
-        boolean isConnected = false;
-
         return isConnected;
     }
 
     // return the esp32 device name
     public String readName() {
-        String name = "";
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return "";
+        }
 
-        return name;
+        return device.getName();
     }
 
     // read wifi names from the esp32
@@ -297,17 +268,3 @@ public class BluetoothManager extends AppCompatActivity {
 
 
 }
-
-//#define SERVICE_UUID_SCAN             "e1f6fa49-170d-4629-bd77-ea170a0309dc"
-//#define CHARACTERISTIC_UUID_SCAN      "27ce423b-bcc6-4fe7-aa98-0d502e79f15a"
-//#define CHARACTERISTIC_UUID_SCAN_NAME "9a61b953-5e81-45f7-b998-4c54d213e710"
-//#define CHARACTERISTIC_UUID_SCAN_NEW  "ffeca8e2-4285-4949-9819-66364ef72d25"
-//
-//#define SERVICE_UUID_WIFI_SELECT           "c3313912-4f44-4c5d-abe4-6a99dbe5274f"
-//#define CHARACTERISTIC_UUID_WIFI_NAME      "62a140cc-0bcf-49da-94e3-fba705938272"
-//#define CHARACTERISTIC_UUID_WIFI_PASSWORD  "1603d3a3-7500-4e90-8310-6e7b12e7bfa4"
-//#define CHARACTERISTIC_UUID_WIFI_SET       "1675752a-2a8e-48e8-99ef-ee90ff878d0f"
-//#define CHARACTERISTIC_UUID_WIFI_CONNECTED "792920e9-f1ff-4587-879a-8cefb09c18d2"
-//
-//#define SERVICE_UUID_CONNECT          "a676b1bc-61f9-4af6-8dae-28831bad2ec6"
-//#define CHARACTERISTIC_UUID_DEVICE    "9fbf707f-c39a-418a-828c-6e291af51ac4"
